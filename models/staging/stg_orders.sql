e4bbef8af8a2743e6f95e7f5b8ea0f861e9dfb00
josecarlos.ico@gmail.com <josecarlos.ico@gmail.com>
2022-06-24 00:44:57 +0000

    added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,22 +1,22 @@
 select 
- a.orderid,
- a.orderdate,
- a.shipdate,
- a.ordersellingprice,
- a.ordercostprice,
- a.ordersellingprice - a.ordercostprice as orderprofit,
- c.customerid,
+a.orderid,
+a.orderdate,
+a.shipdate,
+a.ordersellingprice,
+a.ordercostprice,
+a.ordersellingprice - a.ordercostprice as orderprofit,
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
- 
- 
- from {{ ref('raw_order') }} as a
- left join {{ref('raw_customer')}} as c
- on a.customerid = c.customerid
- left join {{ref('raw_products')}} as p
- on a.productid = p.productid
+p.subcategory,
+{{ markup('ordersellingprice','ordercostprice') }} as markup
+
+from {{ ref('raw_order') }} as a
+left join {{ref('raw_customer')}} as c
+on a.customerid = c.customerid
+left join {{ref('raw_products')}} as p
+on a.productid = p.productid
